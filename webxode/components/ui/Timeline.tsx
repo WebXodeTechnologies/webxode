45f0795ebb55bb4a9c26b4ce069f93ef9e89b45d
ak220193 <mail2meak22frcrio@gmail.com>
2025-01-08 12:49:17 +0530

    changes

--- webxode/components/ui/Timeline.tsx
+++ webxode/components/ui/Timeline.tsx
@@ -52,7 +52,6 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
         </p>
       </div>
 
-
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
         {data.map((item, index) => (
           <div
