8464fb96a477133bbf4d6b145a1e27100f2ab0a4
ak220193 <mail2meak22frcrio@gmail.com>
2025-01-28 21:30:38 +0530

    New pages Added in Routes

---
+++ webxode/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import ComingSoon from '@/components/CommingSoon'
+import React from 'react'
+
+const page = () => {
+  return (
+    <section>
+        <ComingSoon/>
+    </section>
+  )
+}
+
+export default page
