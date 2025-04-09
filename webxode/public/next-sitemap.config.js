2d9e2d3f8c1c0aad656c1ec2a1230a621f5d8938
ak220193 <mail2meak22frcrio@gmail.com>
2025-04-09 19:05:58 +0530

    Config file added for indexing in google console

---
+++ webxode/public/next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: 'https://webxode.vercel.app', // Your live site URL
+    generateRobotsTxt: true,
+    changefreq: 'weekly',
+    priority: 0.7,
+    sitemapSize: 7000,
+    exclude: ['/server-sitemap.xml'], // Optional
+  };
