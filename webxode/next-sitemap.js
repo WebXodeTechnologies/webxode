077ac63542681864d46fcd281ff8cfef81b9affb
ak220193 <mail2meak22frcrio@gmail.com>
2025-05-08 00:12:42 +0530

    sitemap added again with lower case

--- webxode/next-sitemap.js
+++ webxode/next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: 'https://webxode.vercel.app', // Replace with your website's URL
+    siteUrl: 'https://webxode.vercel.app/', // Replace with your website's URL
     generateRobotsTxt: true, // Optional: To automatically generate robots.txt
     sitemapSize: 7000, // Optional: Size of the sitemap (in number of URLs)
   };
